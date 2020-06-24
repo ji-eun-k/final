ff26b9b557f4a13288e46e6bf848cfe3d86d2350
kje <jeun313@gmail.com>
2020-06-24 10:22:35 +0900

    final

--- finalexam01.py
+++ finalexam01.py
@@ -5,8 +5,9 @@ def pipi(i):
     sum = 0.0
     for j in range(1, i, 1):
         sum = sum + (-1) ** (j + 1) / (2 * j - 1)
-    print("{:4}     {:.4F}".format(i, 4 * sum))
+    return sum
 
 print("{:4}     {:4}".format('i', 'mi(i)'))
-for i in range(2,902,100) :
-    pipi(i)
+for i in range(2,1002,100) :
+    sum = pipi(i)
+    print("{:4}     {:.4F}".format(i-1, 4 * sum))
